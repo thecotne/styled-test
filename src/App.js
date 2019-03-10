8ba3214f98e432e856dc1638948f9d80fd256f73
cotne nazarashvili <cotne.95@gmail.com>
2019-03-10 14:37:53 +0400

    use styled-components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled, { keyframes } from 'styled-components'
+import logo from './logo.svg'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+export default function App () {
+  return (
+    <AppContainer>
+      <AppHeader>
+        <AppLogo src={logo} alt="logo" />
+        <AppParagraph>
+          Edit <AppCode>src/App.js</AppCode> and save to reload.
+        </AppParagraph>
+        <AppLink
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </AppLink>
+      </AppHeader>
+    </AppContainer>
+  )
 }
 
-export default App;
+const AppContainer = styled.div`
+  text-align: center;
+`
+
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
+
+const AppLogoSpin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const AppLogo = styled.img`
+  animation: ${AppLogoSpin} infinite 20s linear;
+  height: 40vmin;
+  pointer-events: none;
+`
+
+const AppParagraph = styled.p``
+
+const AppCode = styled.code`
+  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New", monospace;
+
+  ${AppParagraph}:hover & {
+    color: #61dafb;
+  }
+`
+
+const AppLink = styled.a`
+  color: #61dafb;
+`
